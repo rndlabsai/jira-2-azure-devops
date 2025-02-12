e7108e4d479c2c7c0a90f1836fa11b5550ba875a
Frederick Balderrama <antoniobalderramamendoza@gmail.com>
2025-02-12 11:44:23 -0400

    Added basic functionality to store credentials and verify password and username validity

---
+++ login.js
@@ -0,0 +1,59 @@
+import { error } from 'console';
+import fs from 'fs';
+
+const passwordIsValid = (password) => {
+    if (password.length < 8) {
+        return false;
+    }
+
+    if (!/\d/.test(password)) {
+        return false;
+    }
+
+    if (!/[a-zA-Z]/.test(password)) {
+        return false;
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        return false;
+    }
+
+    return true;
+}
+
+const isUsernameUnique = (username) => {
+    const filepath = './credentials.json';
+    let data = { credentials: [] };
+
+    if (fs.existsSync(filepath)) {
+        data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    }
+
+    return data.credentials.find(credential => credential.username === username) === undefined;
+}
+
+const saveCredentials = (username, password) => {
+    const filepath = './credentials.json';
+    let data = { credentials: [] };
+
+    if (fs.existsSync(filepath)) {
+        data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    }
+
+    data.credentials.push({ username, password });
+
+    fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+export const createAccount = (username, password) => {
+    if (!isUsernameUnique(username)) {
+        throw new Error('Username already exists...');
+    }
+
+    if (!passwordIsValid(password)) {
+        throw new Error('Password is invalid...');
+    }
+
+    saveCredentials(username, password);
+    console.log("Account created succesgfully...");
+}
