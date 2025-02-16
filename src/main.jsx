ebe24e0160e84be522b89d1cb936469ccc6922cd
adrian <ferrufinoadrian70@gmail.com>
2025-02-16 14:01:25 -0400

    Added header

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Login from './components/login.jsx'
 import Home from './components/home.jsx'
+import Header from './components/header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login></Login>
-    {/*<Home/>*/}
+    <Header/>
+    <Home/>
   </StrictMode>
 )
