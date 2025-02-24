cafad66d805fd2219c823b7aa888658f7c946e18
mauri-antezana <165099222+mauri-antezana@users.noreply.github.com>
2025-02-24 00:57:35 -0400

    Reform the layout in the migrate page

--- src/components/migrate.jsx
+++ src/components/migrate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './migrate.css';
 import '../styles/global.css';
+import imageMigrate from "../assets/migrate-image.png";
 
 const Migrate = () => {
     const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
@@ -12,34 +13,40 @@ const Migrate = () => {
     return (
         <div className="layout">
             <div className='left-container'>
-
+                <img src={imageMigrate}/>
             </div>
 
             <div className="right-container">
-                <h2>Jira Project:</h2>
+                <label class="migrate-text">Jira Project:</label>
                 <select className="combo-box">
                     <option value="option1">Option 1</option>
                     <option value="option2">Option 2</option>
                 </select>
-                <h2>Azure Project:</h2>
+                <label class="migrate-text">Azure Project:</label>
                 <select className="combo-box">
                     <option value="option1">Option 1</option>
                     <option value="option2">Option 2</option>
                 </select>
-                <button onClick={toggleAdvancedOptions} className='advanced-button'>
-                    {showAdvancedOptions ? 'Hide Advanced Options' : 'Show Advanced Options'}
-                </button>
+                <div className="button-container">
+                    <button onClick={toggleAdvancedOptions} className='button-blue'>
+                        {showAdvancedOptions ? 'Hide' : 'Advanced'}
+                    </button>
+                    <button className="button-blue">Migrate</button>
+                </div>
                 <div className={`advanced-options ${showAdvancedOptions ? 'show' : ''}`}>
                     <label>
                         <input type="checkbox" />
-                        Advanced Option 1
+                        Custom Fields
                     </label>
                     <label>
                         <input type="checkbox" />
-                        Advanced Option 2
+                        Issues
                     </label>
-            </div>
-            <button className="button">Migrate</button>
+                    <label>
+                        <input type="checkbox" />
+                        Screens
+                    </label>
+                </div>
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
     {/*<Login />*/}
     
     <Header />
-    <TokenManager />
+    <Migrate />
 
   </StrictMode>
 )
