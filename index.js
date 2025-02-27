14b92c3044be3d6ca3bcf75162ab926806877880
andresGott <ag.gott@gmail.com>
2025-02-27 09:07:01 -0400

    modification of index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ async function main() {
     try {
        
          // Intentamos registrar un usuario
-         await registerUser('andres123', 'SecurePass123!')
+         await registerUser('demo45', 'Secur3Pass123!')
          .then(() => console.log("Usuario registrado con éxito."))
          .catch(err => console.log(err.message));
 
         // Intentamos hacer login
-        const user = await loginUser('andres123', 'SecurePass123!');
+        const user = await loginUser('demo45', 'Secur3Pass123!');
         console.log('Bienvenido:', user.username);
     } catch (error) {
         console.error('Error:', error.message);
