ce531b8ff8f5725a64dbebbecc20c8aca18f8fd6
adrian <ferrufinoadrian70@gmail.com>
2025-02-28 11:29:49 -0400

    Modified App.jsx for login to appear first, also default route is login now

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import './App.css'
-import Login from './components/login.jsx'
-import Migrate from './components/migrate.jsx'
-import Header from './components/header.jsx'
-import TokenManager from './components/token-manager.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //Importante esto
-
+import './App.css';
+import Login from './components/login.jsx';
+import Migrate from './components/migrate.jsx';
+import Header from './components/header.jsx';
+import TokenManager from './components/token-manager.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 function App() {
-
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/migrate" element={<Migrate />} />
-        <Route path="/token-manager" element={<TokenManager />} />
+        <Route path="/" element={<Login />} />
+        <Route element={
+          <>
+            <Header />
+            <Outlet />
+          </>
+        }>
+          <Route path="/migrate" element={<Migrate />} />
+          <Route path="/token-manager" element={<TokenManager />} />
+        </Route>
+        {/*Lo que no esta definido va a login*/}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
