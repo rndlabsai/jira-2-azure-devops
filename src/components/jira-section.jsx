e3d7b74c2d4f8b98d98b3c4206886f47bf76f246
Frederick Balderrama <antoniobalderramamendoza@gmail.com>
2025-03-03 12:41:17 -0400

    Added jira api token send to server logic, post jira tokens function, start migration post signal and jira projects retrieval

--- src/components/jira-section.jsx
+++ src/components/jira-section.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
-import './jira-section.css';
-import '../styles/global.css';
+import React, { useEffect } from "react";
+import "./jira-section.css";
+import "../styles/global.css";
+import { postJiraTokens } from "../../utils/api.js";
 
 function JiraSection() {
+  const [hasData, setHasData] = useState(false);
+
+  useEffect(async () => {
+    if (!hasData) {
+      return;
+    }
+
+    const api_token = Document.getElementById("api-token").value;
+
+    if (api_token === "") {
+      alert("API Token is required");
+      setHasData(false);
+      return;
+    }
+
+    const email = Document.getElementById("email").value;
+
+    if (email === "") {
+      alert("Email is required");
+      setHasData(false);
+      return;
+    }
+
+    const url = Document.getElementById("url").value;
+
+    if (url === "") {
+      alert("URL is required");
+      setHasData(false);
+      return;
+    }
+
+    const response = await postJiraTokens(api_token, email, url);
+    if (response === false) {
+      alert("Your API TOKEN seems to be invalid or have expired...");
+      setHasData(false);
+      return;
+    }
+
+    if (response === true) {
+      alert("Your Jira information has been saved successfully!");
+      return;
+    }
+  }, [hasData]);
+
   return (
     <div className="jira-section">
       <h2>Jira</h2>
@@ -14,14 +59,12 @@ function JiraSection() {
           placeholder="Enter your Jira API token"
         />
 
-        
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           id="email"
           placeholder="Enter the email you use in Jira"
         />
-
 
         <label htmlFor="url">URL:</label>
         <input
@@ -30,7 +73,9 @@ function JiraSection() {
           placeholder="Enter the url of your Jira instance"
         />
       </div>
-      <button className="save-button">Save Jira Token</button>
+      <button className="save-button" onClick={setHasData(true)}>
+        Save Jira Token
+      </button>
     </div>
   );
 }
--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,58 @@ import axios from 'axios';
 
 const API_URL = "http://localhost:4000/api";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: API_URL,
     headers: { "Content-Type": "application/json" }
 });
+
+const postRequest = async (path, body) => {
+    try {
+        const response = await api.post(path, body);
+        return response;
+    }
+    catch (e) {
+        return e.response;
+    }
+}
+
+export const postJiraTokens = async (api_token, email, url) => {
+    const response = await postRequest("/jira/tokens", {
+        api_token,
+        email,
+        url
+    });
+
+    if (response.status === 401 && response.data.body.cause === "AUTHENTICATION_FAILED") {
+        return false;
+    }
+
+    if (response.status === 200) {
+        return true;
+    }
+    else {
+        return response.data;
+    }
+}
+
+export const getJiraProjects = async () => {
+    const response = await api.get("/jira/projects");
+
+    if (response.status === 200) {
+        return response.data;
+    }
+    else {
+        return response.data;
+    }
+}
+
+export const startMigration = async () => {
+    const response = await postRequest("/migration", {
+        start: true
+    });
+
+    if (response.status === 200) {
+        return true;
+    }
+    return false;
+}
