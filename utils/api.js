ad6fb23065e188430b96e40afa8de94659aa1218
adrian <ferrufinoadrian70@gmail.com>
2025-03-20 17:07:19 -0400

    Added docker to the frontend

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = "http://backend:4000/api";
 
 // Create an axios instance
 const api = axios.create({
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+ export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Permite acceso desde fuera del contenedor
+    port: 5173,       // Usa el puerto correcto
+    strictPort: true  // Evita que Vite cambie de puerto automáticamente
+}
+});
